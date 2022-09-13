06b4445b20173b2030ddc8db78e5bab432da07e3
Festus Osinachi <connect2festus@gmail.com>
2022-09-13 17:33:32 +0100

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,23 @@
-#include <stdio.h>
+#include "_putchar.h"
+
 /**
- * main - _putchar 
+ * main - Prints _putchar
+ * @void: Empty parameter list for main.
  *
- * Return: Always 0 (Success)
- */
+ * Description: Prints _putchar w/out stdio
+ *
+ * Return: 0 for success
+*/
 int main(void)
 {
-
- system ("echo '_putchar\n' ");
-	
-	return 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
