85d25af3fb4378b640dcdad230b6fda7ebd2448c
Festus Osinachi <connect2festus@gmail.com>
2022-09-13 07:27:00 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+
+/* Below function printsout on the output without using the standard printf */
+
+ system ("echo '_putchar\n' ");
+return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+Write a program that prints _putchar, followed by a new line.
+
+
+
+The program should return 0
