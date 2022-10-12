e77be323288d9349997798bcc4daf5914914fa82
Festus Osinachi <connect2festus@gmail.com>
2022-10-12 23:02:55 +0100

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,28 +1,73 @@
 #include "3-calc.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * get_op_func - returns a pointer to the function that corresponds to the
- * operator given as a parameter
- * @s: operator
- *
- * Return: operation
+ * op_add - adds a and b
+ * @a: first number
+ * @b: second number
+ * Return: sum of a and b
  */
 
-int (*get_op_func(char *s))(int, int)
+int op_add(int a, int b)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i;
+	return (a + b);
+}
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
-		i++;
+/**
+ * op_sub - adds a and b
+ * @a: first number
+ * @b: second number
+ * Return: sum of a and b
+ */
 
-	return (ops[i].f);
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - adds a and b
+ * @a: first number
+ * @b: second number
+ * Return: sum of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - adds a and b
+ * @a: first number
+ * @b: second number
+ * Return: sum of a and b
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - adds a and b
+ * @a: first number
+ * @b: second number
+ * Return: sum of a and b
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
